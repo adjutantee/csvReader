22b041f69441682e876d5854f2e1eaaa3e5fbd9f
adjutantee <romashilov3233@gmail.com>
2022-03-30 10:00:12 +0300

    Добавьте файлы проекта.

---
+++ csvReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using CsvHelper;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var reader = new StreamReader(@"C:\Users\Izagakhmaevra\Desktop\Excel\TestExelCSV.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<Foo>();
+        }
+    }
+
+    public class Foo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    //public static void Print()
+    //{
+    //    Console.WriteLine();
+    //}
+}
